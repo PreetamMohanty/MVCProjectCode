81883c6191e9e764809e11bd3069f3032e0918f6
SunveerReddy <34880809+SunveerReddy@users.noreply.github.com>
2017-12-29 17:27:21 +0530

    Add project files.

---
+++ MVCProject2/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using MVCProject2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string id)
+        {
+            TempData["FirstName"] = id;
+            TempData.Keep("FirstName");
+            return View();
+        }
+
+        public ActionResult Redirect()
+        {
+            return View();
+        }
+        public ActionResult New()
+        {
+            EmpDept ed = new EmpDept { FirstName = "Preetam", DeptName = "ECE" };
+            return View(ed);
+        }
+    }
+}
---
+++ MVCProject2/Models/EmpDept.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCProject2.Models
+{
+    public class EmpDept
+    {
+        Employee e1 = new Employee();
+        public string FirstName
+        {
+            get { return e1.FirstName; }
+            set { e1.FirstName = value; }
+        }
+
+        Department d1 = new Department();
+        public string DeptName
+        {
+            get { return d1.DeptName; }
+            set { d1.DeptName = value; }
+        }
+    }
+}
